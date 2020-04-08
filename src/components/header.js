3940009db37e5b3c9acedac5127ab18efb6567ca
Jeff Will <thedarkbit@gmail.com>
2020-04-08 18:03:42 -0400

    Add header content and base styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,26 +3,13 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+  <header class="header">
+    <Link
+      className="header__brand"
+      to="/"
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+      <span class="sr-only">{siteTitle}</span>
+    </Link>
   </header>
 )
 
